8045bc4fd7283cbbf389b5293b750d64ef09f48b
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2024-02-06 00:08:10 +0300

    feat: scheduled updater

--- src/main/java/ru/skillbox/currency/exchange/util/RepositoryUpdater.java
+++ src/main/java/ru/skillbox/currency/exchange/util/RepositoryUpdater.java
@@ -1,6 +1,7 @@
 package ru.skillbox.currency.exchange.util;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.skillbox.currency.exchange.dto.AllCurrenciesDto;
 import ru.skillbox.currency.exchange.entity.Currency;
@@ -8,6 +9,7 @@ import ru.skillbox.currency.exchange.mapper.CurrencyMapper;
 import ru.skillbox.currency.exchange.repository.CurrencyRepository;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Component
@@ -17,6 +19,7 @@ public class RepositoryUpdater {
     private final XmlParser xmlParser;
     private final CurrencyMapper mapper;
 
+    @Scheduled(fixedRate = 1L, timeUnit = TimeUnit.HOURS)
     public void update() {
         repository.deleteAll();
         AllCurrenciesDto allCurrenciesDto = xmlParser.parseCurrencies();
